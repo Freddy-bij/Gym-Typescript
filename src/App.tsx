a06216fe294a57d89689747765fb797cc69530e9
Freddy Bijanja <freddybijanja31@gmail.com>
2025-09-29 19:05:17 +0200

    add progeress project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { SelectedPage } from '@/shared/types'
 import Home from './scenes/home'
 import Benefits from './scenes/benefits'
+import OurClasses from './scenes/ourClasses'
 
 
 
@@ -32,6 +33,8 @@ const App = () => {
 
    <Home setSelectedPage={setSelectedPage}/>
    <Benefits setSelectedPage={setSelectedPage}/>
+   <OurClasses setSelectedPage={setSelectedPage}/>
+   
     </div>
   )
 }
